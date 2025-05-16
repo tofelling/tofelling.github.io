78ae6a6da1b92a30b8e28f2ba53c7d6998251116
tofalling <3087677495@qq.com>
2025-05-16 19:51:48 +0800

    Site updated: 2025-05-16 19:51:47

--- js/custom.js
+++
@@ -1,8 +0,0 @@
-
-document.addEventListener("DOMContentLoaded", () => {
-  document.body.style.opacity = 0;
-  setTimeout(() => {
-    document.body.style.transition = 'opacity 1s';
-    document.body.style.opacity = 1;
-  }, 100);
-});
